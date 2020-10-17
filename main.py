9905dc6b3fd6afc32f309b09caa0722f4afb238a
Oof4 <67525688+Oof4@users.noreply.github.com>
2020-10-17 15:41:14 +1100

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ amount = int(input('Amount of nitro codes to generate: '))
 value = 1
 while value <= amount:
     code = "https://discord.gift/" + ('').join(random.choices(string.ascii_letters + string.digits, k=16))
-    f = open('Codes.txt', "a+")
+    f = open('VaildCodesFromTheGenerator.txt', "a+")
     f.write(f'{code}\n')
     f.close()
-    print(f'[GENERATED] {code}')
+    print(f'[Generated] [Unchecked] {code}')
     value += 1
